913c1b65ce5a603a991ed61f04ee0b8fe3865af2
Nurana Verdiyeva <nuru@mailbox.unideb.hu>
2021-01-20 00:12:01 +0100

    tasks as of now

---
+++ comparetriplets.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the compareTriplets function below.
+function compareTriplets(a, b) {
+     var a1 = 0, b1 = 0;
+    
+    for (var i = 0; i < a.length; i++){
+        if (a[i] > b[i]){
+            a1 += 1;
+        } else if (a[i] < b[i]){
+            b1 += 1;
+        }
+    }
+    
+    return [a1,b1];
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const b = readLine().replace(/\s+$/g, '').split(' ').map(bTemp => parseInt(bTemp, 10));
+
+    const result = compareTriplets(a, b);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
